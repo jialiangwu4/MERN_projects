5515ba8412f248cc078f8b0920f70f2c0434713c
jialiangwu4 <jialiang.wu4@gmail.com>
2024-11-03 21:30:52 -0600

    edit comment in product.model.js

--- backend/model/product.model.js
+++ backend/model/product.model.js
@@ -1,3 +1,11 @@
+// Mongoose is a MongoDB ORM (Object Relational Mapping) library for Node.js
+// It simplifies interacting with MongoDB and makes it more intuitive and easier to use
+// ORMs provide a level of abstraction between the code and the database, which makes it easier to switch between databases
+// Mongoose does things like:
+//   - Convert between JavaScript data types and the BSON data types used in MongoDB
+//   - Provide a way to define the structure of data in the database (through the use of Schemas)
+//   - Provide an interface to interact with the database (CRUD - Create, Read, Update, Delete) operations
+
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
@@ -14,7 +22,7 @@ const productSchema = new mongoose.Schema({
         required: true
     },    
 }, {
-    timestamps: true  // createdAt and updatedAt. auto generated and updated by mongoose
+    timestamps: true  // createdAt and updatedAt. auto generated and updated by mongoose. this is optional
 });
 
 const Product = mongoose.model("Product", productSchema);
